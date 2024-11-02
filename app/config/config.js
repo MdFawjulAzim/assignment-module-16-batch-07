d4e34c0ec5ee895644f6298f9bf359da62ea9c85
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2024-11-02 16:37:11 +0600

    config fil upload

--- app/config/config.js
+++ app/config/config.js
@@ -1,14 +1,15 @@
-import * as path from "node:path";
 
-export const MONGODB_CONNECTION="";
 
-export const JWT_SECRET = "kgqecnuwef5v7fhkc445fjs";
+export const MONGODB_CONNECTION="mongodb+srv://mrrabbil:mernx123@cluster0.rtpbcy6.mongodb.net/app_todo?retryWrites=true&w=majority";
+
+export const JWT_SECRET = "5EC7CEFA1BE7C9354A639369A2AA8JS";
 export const JWT_EXPIRATION_TIME = 60*60*24*30;
 
-export const EMAIL_HOST = "";
-export const EMAIL_PORT = "";
-export const EMAIL_USER = "";
-export const EMAIL_PASSWORD = "";
+export const EMAIL_HOST = "sandbox.smtp.mailtrap.io";
+export const EMAIL_PORT = "2525";
+export const EMAIL_USER = "36c1cf0b7f496f";
+export const EMAIL_PASSWORD = "91480c498acc52";
+export const MAIL_ENCRYPTION=""; //as needed for the mailtrap
 
 export const Max_JSON_SIZE = "50mb";
 export const URL_ENCODER = true;
@@ -20,7 +21,3 @@ export const REQUEST_LIMIT_NUMBER =3000;//per 15 Min 3000 Requests Allowed
 
 export const WEB_CACHE=false;
 export const PORT=5050;
-
-export function UPLOAD_FOLDER(fileName){
-    return path.resolve(process.cwd(), 'storage', fileName);
-}
