43b1bb34aed2361b46265d9aeb3e8081fb11f532
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2024-11-02 16:40:33 +0600

    controller add

---
+++ app/controllers/blogController.js
@@ -0,0 +1,23 @@
+//create
+
+export const createBlog= async (req,res)=>{
+    return res.json({message:'Blog created successfully!'})
+}
+
+//read
+
+export const readBlog = async (req,res)=>{
+    return res.json({message:'Blog read successfully!'})
+}
+
+//create
+
+export const updateBlog = async (req,res)=>{
+    return res.json({message:'Blog update successfully!'})
+}
+
+//create
+
+export const deleteBlog = async (req,res)=>{
+    return res.json({message:'Blog delete successfully!'})
+}
